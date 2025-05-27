49663274aa5953317046f4a855752f4b21cf49db
Matvei Ivanov <matveyivanov@10.208.178.148-spb.dhcp.yndx.net>
2025-05-27 09:25:39 +0300

    Removed cloud function call from non-prod environment, added uniquness for locations

--- src/endpoints/locations.py
+++ src/endpoints/locations.py
@@ -1,11 +1,14 @@
 import asyncio
 import json
+from collections.abc import Iterable
 from datetime import datetime
+from typing import TypeVar
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, Request
 from sse_starlette.sse import EventSourceResponse
 
+from config import settings
 from config.di import Container
 from schemas.locations import IPEvent, Locations
 from services.interfaces import IRingBuffer
@@ -15,6 +18,17 @@ router = APIRouter(prefix="/locations", tags=["locations"])
 MESSAGE_STREAM_DELAY = 1  # second
 LOCATIONS_KEY = "LOCATIONS"
 
+T = TypeVar("T", bound=dict)
+
+
+def unique_dict(objs: Iterable[T], *, key: str) -> list[T]:
+    seen, result = set(), list()
+    for obj in objs:
+        if obj[key] not in seen:
+            seen.add(obj[key])
+            result.append(obj)
+    return result
+
 
 @router.get("", response_model=Locations)
 @inject
@@ -23,7 +37,7 @@ async def locations(
     ring_buffer: IRingBuffer[dict] = Depends(Provide[Container.locations_ring_buffer]),
     queue=Depends(Provide[Container.ymq_locations_queue]),
 ):
-    if request.client:
+    if request.client and settings.PROD:
         queue.send_message(
             MessageBody=json.dumps(
                 IPEvent(
@@ -33,7 +47,7 @@ async def locations(
             ),
             MessageGroupId="default",
         )
-    return {"locations": await ring_buffer.all()}
+    return {"locations": unique_dict(await ring_buffer.all(), key="location")}
 
 
 @router.get("/stream")
@@ -49,7 +63,9 @@ async def stream(
 
             yield {
                 "event": "message",
-                "data": json.dumps({"locations": await ring_buffer.all()}),
+                "data": json.dumps(
+                    {"locations": unique_dict(await ring_buffer.all(), key="location")}
+                ),
             }
 
             await asyncio.sleep(MESSAGE_STREAM_DELAY)
