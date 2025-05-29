d31cfd49a5e8cb699d5f09652387b78610c2a8e4
Matvei Ivanov <matveyivanov@10.208.178.148-spb.dhcp.yndx.net>
2025-05-29 10:11:53 +0300

    Better formatting

--- f-locations/src/config.py
+++ f-locations/src/config.py
@@ -21,10 +21,16 @@ REDIS_HOST: str = load("REDIS_HOST", "redis", ensure_not_empty=True)
 REDIS_PORT: int = load("REDIS_PORT", 6379, cast_to=int, ensure_not_empty=True)
 REDIS_PASSWORD: str = load("REDIS_PASSWORD", "password", ensure_not_empty=True)
 REDIS_SOCKET_TIMEOUT: int = load(
-    "REDIS_SOCKET_TIMEOUT", 5, cast_to=int, ensure_not_empty=True
+    "REDIS_SOCKET_TIMEOUT",
+    5,
+    cast_to=int,
+    ensure_not_empty=True,
 )
 REDIS_SOCKET_CONNECTION_TIMEOUT: int = load(
-    "REDIS_SOCKET_CONNECTION_TIMEOUT", 5, cast_to=int, ensure_not_empty=True
+    "REDIS_SOCKET_CONNECTION_TIMEOUT",
+    5,
+    cast_to=int,
+    ensure_not_empty=True,
 )
 
 IPINFO_ACCESS_TOKEN: str = load("IPINFO_ACCESS_TOKEN", "")
@@ -32,11 +38,18 @@ IPINFO_TIMEOUT: int = load("IPINFO_TIMEOUT", 5, cast_to=int, ensure_not_empty=Tr
 IPINFO_DEFAULT_CITY: str = load("IPINFO_DEFAULT_CITY", "Unknown", ensure_not_empty=True)
 
 LOCATIONS_BUFFER_NAME: str = load(
-    "LOCATIONS_BUFFER_NAME", "locations", ensure_not_empty=True
+    "LOCATIONS_BUFFER_NAME",
+    "locations",
+    ensure_not_empty=True,
 )
 LOCATIONS_BUFFER_MAX_SIZE: int = load(
-    "LOCATIONS_BUFFER_MAX_SIZE", 5, cast_to=int, ensure_not_empty=True
+    "LOCATIONS_BUFFER_MAX_SIZE",
+    5,
+    cast_to=int,
+    ensure_not_empty=True,
 )
 LOCATIONS_HASHSET_NAME: str = load(
-    "LOCATIONS_HASHSET_NAME", "hashset:locations", ensure_not_empty=True
+    "LOCATIONS_HASHSET_NAME",
+    "hashset:locations",
+    ensure_not_empty=True,
 )
