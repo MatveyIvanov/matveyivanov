a9f90372e7d9b6cde75c24d3a08701ac43faad8b
Matvey Ivanov <58879691+MatveyIvanov@users.noreply.github.com>
2025-06-11 23:10:30 +0300

    Removed translation utils (#83)

--- src/utils/shortcuts.py
+++ src/utils/shortcuts.py
@@ -1,9 +1,8 @@
-from config.i18n import _
 from utils.exceptions import Custom404Exception
 
 
 def get_object_or_404[T](obj: T | None, *, msg: str | None = None) -> T:
-    msg = _(msg) if msg else _("Not found.")
+    msg = msg or "Not found."
     if obj is None:
         raise Custom404Exception(msg)
     return obj
